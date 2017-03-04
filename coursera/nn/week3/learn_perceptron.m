14549a86b4ed178bd4cd17c6dfe443229e095827
Evan Jenkins <evan@earlydata.com>
2017-03-04 16:25:02 +0800

    coursera/nn week 3 finished

--- coursera/nn/week3/learn_perceptron.m
+++ coursera/nn/week3/learn_perceptron.m
@@ -111,7 +111,9 @@ for i=1:num_neg_examples
     x = this_case'; %Hint
     activation = this_case*w;
     if (activation >= 0)
-        %YOUR CODE HERE
+       %YOUR CODE HERE%
+       w = w - x 
+       %%%%%%%%%%%%%%%%
     end
 end
 for i=1:num_pos_examples
@@ -119,7 +121,9 @@ for i=1:num_pos_examples
     x = this_case';
     activation = this_case*w;
     if (activation < 0)
-        %YOUR CODE HERE
+        %YOUR CODE HERE%
+        w = w + x
+        %%%%%%%%%%%%%%%%
     end
 end
 
